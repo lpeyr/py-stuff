8deefc7aa151507d06d6e9b4719eda1ddbf42383
Léo Peyronnet <leo.peyronnet@outlook.fr>
2024-11-08 11:42:33 +0100

    Added TP6

---
+++ INSA/1A/TD6/ex5.py
@@ -0,0 +1,20 @@
+voyelles = ("a","e","i","o","u","y")
+mot = "mercredi"
+
+def compter_voyelle(mot):
+    n = 0
+    for lettre in mot:
+        if lettre in voyelles:
+            n += 1
+    return n
+
+def position_voyelle(mot):
+    voy = []
+    for i in range(len(mot)):
+        if mot[i] in voyelles:
+            voy.append((mot[i], i))
+    return voy
+
+print(f"Le mot {mot} contient les voyelles suivantes :")
+for element in position_voyelle(mot):
+    print(f"-> {element[0]} à la position {element[1]}")
---
+++ INSA/1A/TD6/ex6.py
@@ -0,0 +1,18 @@
+def cherche_min(liste, i_pivot):
+    min = liste[i_pivot]
+    i_min = i_pivot
+    for i in range(i_pivot, len(liste)):
+        if liste[i] < min:
+            min = liste[i]
+            i_min = i
+    return i_min
+
+def tri_selection(liste):
+    for i_pivot in range(len(liste)):
+        i_min = cherche_min(liste,i_pivot)
+        if liste[i_pivot] > liste[i_min]:
+            liste[i_pivot], liste[i_min] = liste[i_min], liste[i_pivot]
+
+    return liste
+
+print(tri_selection([3,2,1]))
