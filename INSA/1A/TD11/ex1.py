318455cedabbe69518eeca02299283da1cc04b64
Léo Peyronnet <leo.peyronnet@outlook.fr>
2024-12-20 11:39:43 +0100

    Added TD11

---
+++ INSA/1A/TD11/ex1.py
@@ -0,0 +1,57 @@
+def calcul_impot(revenu, nb_parts):
+    """
+    Entrées :
+        revenu   (int) : Le revenu du ménage
+        nb_parts (int) : Nombre de parts
+    Retour : L'impot du sur le revenu
+    """
+    ratio = revenu/nb_parts
+    if ratio < 20000:
+        impot = revenu*0.125 - 50*nb_parts
+    elif ratio < 30000:
+        impot = revenu*0.25 - 100*nb_parts
+    else:
+        impot = revenu*0.5 - 200*nb_parts
+
+    return impot
+
+def afficher_foyer(revenus, parts, impots):
+    """
+    Entrées :
+        revenus (list[int]) : les revenus des foyers
+        parts   (list[int]) : les parts des foyers
+        impots  (list[int]) : les impots payées par le foyer
+    Retour :
+        (None) Affiche les revenus des foyers.
+    """
+    for i in range(len(revenus)):
+        print(f"Avec un revenu de {revenus[i]}€ et {parts[i]} parts, un foyer paiera {impots[i]}€.")
+
+
+def saisir_un_foyer():
+    rev, parts = int(input("Entrez un revenu\n>")), int(input("Entrez le nombre de parts\n>"))
+    while rev != -1 and (rev < 0 or parts < 0):
+        rev, parts = int(input("Entrez un revenu")), int(input("Entrez le nombre de parts"))
+
+    return rev, parts
+
+ # Tests
+print(f"revenu : 25000 - parts : 2, l’impot vaut : {calcul_impot(25000,2)}")
+print(f"revenu : 80000 - parts : 3, l’impot vaut : {calcul_impot(80000,3)}")
+print(f"revenu : 130000 - parts : 4, l’impot vaut : {calcul_impot(130000,4)}")
+
+les_revenus = [25000, 80000, 130000, 75000, 500, 30000]
+les_parts = [2, 3, 4, 3, 1, 5]
+les_impots = []
+
+for i in range(len(les_revenus)):
+    les_impots.append(calcul_impot(les_revenus[i], les_parts[i]))
+
+afficher_foyer(les_revenus, les_parts, les_impots)
+
+rev, parts = saisir_un_foyer()
+while rev != -1:
+    les_revenus.append(rev)
+    les_parts.append(parts)
+    rev, parts = saisir_un_foyer()
+
---
+++ INSA/1A/TD11/ex2.py
@@ -0,0 +1,88 @@
+from math import sqrt
+
+def est_divisible(n, p) :
+    """ 
+    Renvoie vrai si et seulement si n est divisible par p. 
+    
+    Parametres
+    ----------
+    n, p : deux entiers
+
+    Retour
+    -------
+    True ou False
+    """
+    return n % p == 0
+
+
+def get_diviseurs_stricts(n) :
+    """ 
+    Renvoie la liste des diviseurs stricts de n.
+    Parametres
+    ----------
+    n : un entier
+
+    Retour
+    -------
+    liste des diviseurs stricts de n (liste d'entiers).
+    """
+    divs = []
+    j = 0
+    for p in range(1, n // 2 + 1):
+        if est_divisible(n, p):
+            divs.append(p)
+        j += 1
+    print(j)
+    return divs, j
+    
+
+
+def est_parfait(n) :
+    """ Renvoie vrai si et seulement si n est un nombre parfait. 
+    Parametres
+    ----------
+    n : un entier
+    
+    Retour
+    -------
+    Vrai ou Faux
+    """
+    s = 0
+    nbs, j = get_diviseurs_stricts(n)
+    for nb in nbs:
+        s += nb
+
+    return s == n
+
+def parfait_range(a, b):
+    """
+    Entrées :
+        a (int) : Nombre de départ de l'intervalle
+        b (int) : Nombre maximal de l'intervalle
+    Retourne :
+        La liste des nombres parfaits dans l'intervalle [a, b]
+    """
+    parfaits = []
+    for p in range(a, b + 1):
+        if est_parfait(p):
+            parfaits.append(p)
+    return parfaits
+
+### Programme principal
+# Test de est_divisible
+#print(est_divisible(10, 2)) # True
+#print(est_divisible(10, 3)) # False
+#print(est_divisible(10, 0)) # Erreur
+
+# Test de get_diviseurs_stricts
+
+#print(get_diviseurs_stricts(10)) # [1, 2, 5]
+#print(get_diviseurs_stricts(3)) # [1]
+
+
+# Test de est_parfait
+
+print(est_parfait(6)) # True
+print(est_parfait(5)) # False
+
+print(parfait_range(0, 500)) # \sum_{k=a}^b\left\lfloor \frac{k}{2} \right\rfloor\newline=\=\frac{b(b+1)}{4}-\frac{a(a+1)}{4}=\frac{b²+b-a²-a}{4}=\frac{(b-a)(b+a+1)}{4}
